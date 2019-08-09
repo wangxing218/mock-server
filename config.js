91cf358aac1f7c78407569d9b6c45a1b107d9dd3
xingwang4 <xingwang4@iflytek.com>
2019-08-09 14:01:25 +0800

    修改默认配置端口和延时

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ module.exports = {
   // 域名
   host: '127.0.0.1',
   // 端口
-  port: 80,
+  port: 8008,
   // 延时，单位 ms, 因为响应太快，可以让数据返回延时哦
-  delay: [0, 0],
+  delay: [0, 600],
   // 是否允许跨域
   crossOrigin: true,
 }
